c111db86c054988231b7f3583e03cc6b0c715589
www.seok.com <qhtjr7583@naver.com>
2023-06-29 17:33:32 +0900

    (feat) GridView
    set to Image.network

--- lib/main.dart
+++ lib/main.dart
@@ -296,23 +296,44 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(context.watch<Store2>().name),),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          CircleAvatar(
-            radius: 30,
-            backgroundColor: Colors.grey,
+      body: CustomScrollView(
+        slivers:[
+          SliverToBoxAdapter(
+            child: ProfileHeader(),
           ),
-          Text('팔로워 ${context.watch<Store1>().follower}명'),
-          ElevatedButton(onPressed: (){
-            context.read<Store1>().addFollower();
-          }, child: Text('팔로우')),
-          ElevatedButton(onPressed: (){
-            context.read<Store1>().getData();
-          }, child: Text('사진 가져오기')),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                  (context,i) => Image.network(context.watch<Store1>().profileImage[i]),
+                  childCount: context.watch<Store1>().profileImage.length,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3)
+          )
         ],
-      ),
+      )
     );
   }
 }
 
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.grey,
+        ),
+        Text('팔로워 ${context.watch<Store1>().follower}명'),
+        ElevatedButton(onPressed: (){
+          context.read<Store1>().addFollower();
+        }, child: Text('팔로우')),
+        ElevatedButton(onPressed: (){
+          context.read<Store1>().getData();
+        }, child: Text('사진 가져오기')),
+      ],
+    );
+  }
+}
